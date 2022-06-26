c4af106fc23303ca10db4f81da7fea027323dc88
Michael Braverman <michael.braverman@clearpoint.co.nz>
2022-06-26 15:27:47 +1200

    redundant saving deleted

--- PRO506/Program.cs
+++ PRO506/Program.cs
@@ -214,12 +214,11 @@ namespace Task
                 Console.WriteLine(((Employee)employee).PrintPayrollDetails());//Print their payroll
             }
             else
+            {
                 //Otherwise print invalid id
                 Console.WriteLine("INVALID ID!");
-            //If user pressed s then saved other wise returns to menu
-            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE OR S TO SAVE!");
-            if (Console.ReadKey().Key == ConsoleKey.S)//If key pressed is S
-                SaveData();//Save the data
+            }
+            Console.WriteLine("\nPRESS ANY KEY TO CONTINUE.");
         }
 
         //Print all employees in form of a table
@@ -271,25 +270,6 @@ namespace Task
             return null;//Else return null
         }
 
-        /*
-         * Save the employee in weekly_payroll.txt and file
-         * modes are OpenOrCreate and write.
-         * After writing close the file
-         */
-        public void SaveData()
-        {
-            FileStream fileStream = new FileStream("weekly_payroll.txt", FileMode.OpenOrCreate, FileAccess.Write);//Create file reader with open, create and write
-            StreamWriter streamWriter = new StreamWriter(fileStream);//Create file writer
-            foreach (Employee employee in _employees)//For employee in employees
-            {
-                streamWriter.Write(employee.PrintEmployeeDetails() + "\n");//Write employee in the file
-                streamWriter.Write(employee.PrintPayrollDetails() + "\n");//Write payroll in the file
-            }
-            streamWriter.Close();//Close the writer
-            fileStream.Close();//Close the file
-            Console.Clear();//Clear the console
-            Console.WriteLine("Saved Successfully!");//Print message
-        }
 
         //Print the title
         public static string PrintTitle(string title)
